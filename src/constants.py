b9ef6db2864d29d595db7eed6ea70c232368357d
Evan <evanmdoyle@gmail.com>
2020-03-28 22:40:12 -0700

    Serve other category cards from build folder

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,11 @@ CARD_FILE_NAMES = [
     r"keycard.jpeg",
     r"chameleon_card.jpeg",
     r"arts.jpeg",
+    r"games.jpeg",
+    r"hobbies.jpeg",
+    r"movies.jpeg",
+    r"music.jpeg",
+    r"transport.jpeg"
     # TODO: other categories
 ]
 CARD_PATHS = r"/(" + r"|".join(CARD_FILE_NAMES) + r")"
